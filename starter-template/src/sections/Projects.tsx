6e93e4b71c60b60b4f8990557c2d6dc85f8474fd
sugandhanjr <sudandhanjr@gmail.com>
2024-10-20 20:09:32 +0530

    ff

--- starter-template/src/sections/Projects.tsx
+++ starter-template/src/sections/Projects.tsx
@@ -54,9 +54,12 @@ export const ProjectsSection = () => {
       <p className="text-center text-white/60 mt-4"> see how i transformed concepts into engaging digital experience.</p>
       <div className="flex flex-col mt-10">
         {portfolioProjects.map((project) =>(
-          <div key={project.title} className="bg-gray-800 rounded-3xl relative z-0 overflow-hidden after:-z-10 after:content-['']after:absolute after:inset-0 after:outline-2 after:outline afteroutline-offset-2 after:rounded-3xl">
-        <div>
+          <div key={project.title} className="bg-gray-800 rounded-3xl relative z-0 overflow-hidden after:z-10 after:content-['']after:absolute after:inset-0 after:outline-2 after:outline afteroutline-offset-2 after:rounded-3xl
+          after:outline-white/20 p-8">
+        <div className="flex">
+          <div className="bg-gradient-to-r from-emerald-300 to-sky-400 inline-flex  gap-2 font-bold uppercase tracking-widest text-sm text-transparent bg-clip-text"></div>
           <span>{project.company}</span>
+          <span> &bull;</span>
           <span>{project.year}</span>
           </div>
           <h3>{project.title}</h3>
