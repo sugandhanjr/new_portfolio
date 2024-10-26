55d4345eaed1d58c8e2c245eed4e052b4eae79a9
sugandhanjr <sudandhanjr@gmail.com>
2024-10-26 12:13:33 +0530

    2.16.00

--- starter-template/src/components/HeroOrbit.tsx
+++ starter-template/src/components/HeroOrbit.tsx
@@ -2,9 +2,8 @@ import { PropsWithChildren } from "react"
 
 export const HeroOrbit = ({children ,size, rotation }: PropsWithChildren<{size: number;rotation:number;  }>) => {
   return (
-    <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 -z-20">
-
-      <div className="flex items-start justify-start animate-spin[animation-duration:10s]"
+    <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
+      <div className="flex items-start justify-start"
       style={{
         transform: `rotate(${rotation}deg)`,    
         height: `${size}px`,
--- starter-template/src/sections/Hero.tsx
+++ starter-template/src/sections/Hero.tsx
@@ -54,7 +54,7 @@ export const HeroSection = () => {
         <Image src={memojiImage} className="size=[100px]"alt="Person peeking from behind laptop" />
       <div className='bg-gray-950 border-gray-800 px-4 py-1.5 inline-flex items-center gap-4 rounded-lg'>
       <div className='bg-green-500 size-2.5 rounded-full relative '>
-        <div className='bg-green-500 absolute inset-0 rounded-full  animate-ping-large' ></div>
+        <div className='bg-green-500 absolute inset-0 rounded-full animate-ping' ></div>
       </div>
       <div className='text-sm font-medium'>
         Available for new Projects</div>
