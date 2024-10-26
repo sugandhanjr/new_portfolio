16b0e73a468e1f4132dce663dfcbf8a21a1f4257
sugandhanjr <sudandhanjr@gmail.com>
2024-10-26 13:04:05 +0530

    2.34.00

--- starter-template/src/sections/Hero.tsx
+++ starter-template/src/sections/Hero.tsx
@@ -18,35 +18,36 @@ export const HeroSection = () => {
       <div className=' size-[1020px] hero-ring'></div>
       <div className=' size-[1220px] hero-ring'></div>
       <div className=' size-[1420px] hero-ring'></div>
-      <HeroOrbit size={800} rotation={-72}>
-      <StarIcon className=" size-28 text-emerld-300"/>
-      </HeroOrbit>
-      <HeroOrbit size={550} rotation={20}>   
-      <StarIcon className=" size-12 text-emerld-300"/>
-      </HeroOrbit>
-      <HeroOrbit size={590} rotation={ 98}>   
-      <StarIcon className=" size-8  text-emerld-300"/>
-      </HeroOrbit>  
-      <HeroOrbit size={430} rotation={ -14}>   
+      
+      <HeroOrbit size={430} rotation={ -14} shouldOrbit orbitDuration='30s'shouldSpin spinDuration='3s'>   
       <SparkeIcon className=" size-8  text-emerld-300/20"/>
       </HeroOrbit>  
-      <HeroOrbit size={440} rotation={ 79}>   
+      <HeroOrbit size={440} rotation={ 79}shouldOrbit orbitDuration='32s'shouldSpin spinDuration='3s'>   
       <SparkeIcon className=" size-5   text-emerld-300/20"/>
       </HeroOrbit>
-      <HeroOrbit size={530} rotation={ 178}>   
-      <SparkeIcon className=" size-10  text-emerld-300/20"/>
-      </HeroOrbit>
-      <HeroOrbit size={710} rotation={ 144}>   
-      <SparkeIcon className=" size-14  text-emerld-300/20"/>
-      </HeroOrbit>
-      <HeroOrbit size={720} rotation={ 85}>   
-      <SparkeIcon className=" size-3 rounded-full  text-emerld-300/20"/>
-      </HeroOrbit>
-      <HeroOrbit size={520} rotation={ -41}>   
+      <HeroOrbit  size={520} rotation={ -41}shouldOrbit orbitDuration='34s'shouldSpin spinDuration='3s'>   
       <SparkeIcon className=" size-2rounded-full  text-emerld-300/20"/>
       </HeroOrbit>
-      <HeroOrbit size={650} rotation={-5}>   
+      <HeroOrbit size={530} rotation={ 178}shouldOrbit orbitDuration='36s'shouldSpin spinDuration='3s'>   
+      <SparkeIcon className=" size-10  text-emerld-300/20"/>
+      </HeroOrbit>
+      <HeroOrbit size={550} rotation={20}shouldOrbit orbitDuration='38s'shouldSpin spinDuration='6s'>   
+      <StarIcon className=" size-12 text-emerld-300"/>
+      </HeroOrbit>
+      <HeroOrbit size={590} rotation={ 98}shouldOrbit orbitDuration='40s'shouldSpin spinDuration='6s'>   
+      <StarIcon className=" size-8  text-emerld-300"/>
+      </HeroOrbit>  
+      <HeroOrbit size={650} rotation={-5}shouldOrbit orbitDuration='42s'shouldSpin spinDuration='6s'>   
       <SparkeIcon className=" size-2 rounded-full  text-emerld-300/20"/>
+      </HeroOrbit>
+      <HeroOrbit size={710} rotation={ 144}shouldOrbit orbitDuration='44s'shouldSpin spinDuration='6s'>   
+      <SparkeIcon className=" size-14  text-emerld-300/20"/>
+      </HeroOrbit>
+      <HeroOrbit size={720} rotation={ 85}shouldOrbit orbitDuration='46s'shouldSpin spinDuration='6s'>   
+      <SparkeIcon className=" size-3 rounded-full  text-emerld-300/20"/>
+      </HeroOrbit>
+      <HeroOrbit size={800} rotation={-72}shouldOrbit orbitDuration='48s' shouldSpin spinDuration='6s'>
+      <StarIcon className=" size-28 text-emerld-300"/>
       </HeroOrbit>
       </div>
       <div className="cotainer">
@@ -54,7 +55,7 @@ export const HeroSection = () => {
         <Image src={memojiImage} className="size=[100px]"alt="Person peeking from behind laptop" />
       <div className='bg-gray-950 border-gray-800 px-4 py-1.5 inline-flex items-center gap-4 rounded-lg'>
       <div className='bg-green-500 size-2.5 rounded-full relative '>
-        <div className='bg-green-500 absolute inset-0 rounded-full animate-ping' ></div>
+        <div className='bg-green-500 absolute inset-0 rounded-full  animate-ping ' ></div>
       </div>
       <div className='text-sm font-medium'>
         Available for new Projects</div>
